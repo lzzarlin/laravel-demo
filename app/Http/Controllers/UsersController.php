aae2a2478dc3a0449085b1a5cf4079520a766560
lzzarlin <63822013@qq.com>
2020-12-23 07:15:02 +0000

    用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,6 +17,10 @@ class UsersController extends Controller
         ]);
 
     }
+    public function index(){
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
     //
     public function create(){
         return view('users.create');
